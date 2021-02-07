26cd29d09e5c97c1a4ff27178da5354a5901e0f1
Alex Reitz <alexreitz@Alexs-MacBook-Pro.local>
2021-02-07 08:26:26 -0500

    Timer

--- app.py
+++ app.py
@@ -1,31 +1,32 @@
 from boggle import Boggle
-from flask import Flask, render_template, session, jsonify, request, redirect, url_for
+from flask import Flask, render_template, session, jsonify, request, redirect, make_response
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
+CORS(app)
 debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
 Guesses = []
 board = boggle_game.make_board()
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def home_page():
-    session["board"] = board
-    session["guess_tracker"] = Guesses
-    return render_template("home.html", board=board)
-
-@app.route("/", methods=[ 'POST'])
-def guess_to_server():
+    if(request.method == "POST"):
+        req = request.get_json()
         answer = request.form["guess"]
-        print(answer)
         #Save guess in session list
         Guesses = session["guess_tracker"]
         Guesses.append(answer)
         session["guess_tracker"] = Guesses
-        #Check if word is valid
+    #Check if word is valid
         validatedAnswer = boggle_game.check_valid_word(board, answer)
         return jsonify(validatedAnswer)
+    else: 
+        return render_template("home.html", board=board)
+
+   
 
     
--- static/home.js
+++ static/home.js
@@ -4,13 +4,27 @@ $(function () {
     evt.preventDefault();
     const $inputValue = $("#guessValue").val()
     console.log($inputValue);
+    /* Make axios post request to flask server */
     const response = await axios({
-        method: "POST",
-        url: `/stories`,
+        method: "post",
+        url: "localhost:5000/",
         data: {
-          answer: $inputValue
-        },
-      });
-    
+            input: `${$inputValue}`
+            }
+        })
     });
-  });
+});
+
+window.onload = function() {
+    $("#gameOver").hide()
+    let count = 60, timer = setInterval(function() {
+        $("#timer").html(`Time left: ` + count-- + ` seconds`);
+        if(count === -1){
+            clearInterval(timer)
+            $("#guessValue").hide()
+            $("#submit").hide()
+            $("#labelGuess").hide()
+            $("#gameOver").show()
+        }
+    }, 1000);
+  }
