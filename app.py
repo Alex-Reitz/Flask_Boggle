a6be15572a73219d9f6a5757fc9e82041ab5d05b
Alex Reitz <alexreitz@Alexs-MacBook-Pro.local>
2021-02-07 11:07:56 -0500

    async call to server

--- app.py
+++ app.py
@@ -1,32 +1,23 @@
 from boggle import Boggle
 from flask import Flask, render_template, session, jsonify, request, redirect, make_response
-from flask_debugtoolbar import DebugToolbarExtension
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
-CORS(app)
-debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
-Guesses = []
-board = boggle_game.make_board()
 
-@app.route("/", methods=["GET","POST"])
-def home_page():
-    if(request.method == "POST"):
-        req = request.get_json()
-        answer = request.form["guess"]
-        #Save guess in session list
-        Guesses = session["guess_tracker"]
-        Guesses.append(answer)
-        session["guess_tracker"] = Guesses
-    #Check if word is valid
-        validatedAnswer = boggle_game.check_valid_word(board, answer)
-        return jsonify(validatedAnswer)
-    else: 
-        return render_template("home.html", board=board)
+@app.route("/")
+def homepage():
+    """Show board."""
+    board = boggle_game.make_board()
+    session['board'] = board
+    return render_template("home.html", board=board)
 
-   
 
-    
+@app.route("/check-word")
+def check_word():
+    """Check if word is in dictionary."""
+    guess = request.args["guess"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, guess)
+    return jsonify({'result': response})
--- static/home.js
+++ static/home.js
@@ -2,24 +2,27 @@ $(function () {
     // pass the event object as 'evt' to the callback
     $('#submit').click(async function sendAnswer(evt){
     evt.preventDefault();
-    const $inputValue = $("#guessValue").val()
-    console.log($inputValue);
+    let inputValue = $("#guessValue").val()
+    console.log(inputValue);
     /* Make axios post request to flask server */
-    const response = await axios({
-        method: "post",
-        url: "localhost:5000/",
-        data: {
-            input: `${$inputValue}`
-            }
-        })
-    });
+    const resp = await axios.get("/check-word", { params: { inputValue: inputValue }});
+    if (resp.data.result === "not-word") {
+      showMessage(`${inputValue} is not a valid English word`);
+    } else if (resp.data.result === "not-on-board") {
+      showMessage(`${inputValue} is not a valid word on this board`);
+    } 
 });
+
+function showMessage(msg){
+    let message = $("#msg")
+    message.innerText = msg
+}
 
 window.onload = function() {
     $("#gameOver").hide()
     let count = 60, timer = setInterval(function() {
         $("#timer").html(`Time left: ` + count-- + ` seconds`);
-        if(count === -1){
+        if(count === -1) {
             clearInterval(timer)
             $("#guessValue").hide()
             $("#submit").hide()
@@ -27,4 +30,5 @@ window.onload = function() {
             $("#gameOver").show()
         }
     }, 1000);
-  }
+}
+})
